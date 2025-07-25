51ec53373f7bd01af83c22a4ed63eab42b7f974d
Adrian <aortizlao4@gmail.com>
2025-07-25 18:17:59 -0300

    pdfnuevo

--- hooks/usePdfGenerator.ts
+++ hooks/usePdfGenerator.ts
@@ -74,7 +74,7 @@ export function usePdfGenerator(): UsePdfGeneratorReturn {
         const pageData = pageCanvas.toDataURL('image/png');
         const pageHeightMm = pageCanvasHeight / pxPerMm;
         if (printedHeight > 0) {
-          pdf.addPage();
+          pdf.addPage('a4', 'p');
         }
         pdf.addImage(pageData, 'PNG', 0, 0, pdfWidth, pageHeightMm);
         printedHeight += pageCanvasHeight;
